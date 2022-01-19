9d1523bbed52b907ec25cef6079217de3c7b8733
Amanda Ki <aki18@uw.edu>
2022-01-19 11:38:41 -0800

    Add build artifacts

---
+++ app/src/main/java/edu/washington/activityspy/MainActivity.kt
@@ -0,0 +1,50 @@
+package edu.washington.activityspy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Greetings, UW!", Toast.LENGTH_LONG).show()
+
+        Log.i(TAG, "onCreate(savedInstanceState) event fired\nAdditional data with onCreate: "
+                + savedInstanceState.toString())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart() event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume() event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause() event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop() event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy() event fired")
+        Log.e(TAG, "We're going down, Captain!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart() event fired")
+    }
+}
