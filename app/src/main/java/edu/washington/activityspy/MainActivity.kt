bb517517cab9da39e267f905dec5a21ac92e62e9
Amanda Ki <aki18@uw.edu>
2022-01-19 11:39:27 -0800

    Add screenshots directory

--- app/src/main/java/edu/washington/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/activityspy/MainActivity.kt
@@ -13,38 +13,38 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, "Greetings, UW!", Toast.LENGTH_LONG).show()
 
-        Log.i(TAG, "onCreate(savedInstanceState) event fired\nAdditional data with onCreate: "
+        Log.i(TAG, "onCreate event fired\nAdditional data with onCreate: "
                 + savedInstanceState.toString())
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i(TAG, "onStart() event fired")
+        Log.i(TAG, "onStart event fired")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i(TAG, "onResume() event fired")
+        Log.i(TAG, "onResume event fired")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i(TAG, "onPause() event fired")
+        Log.i(TAG, "onPause event fired")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i(TAG, "onStop() event fired")
+        Log.i(TAG, "onStop event fired")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(TAG, "onDestroy() event fired")
+        Log.i(TAG, "onDestroy event fired")
         Log.e(TAG, "We're going down, Captain!")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i(TAG, "onRestart() event fired")
+        Log.i(TAG, "onRestart event fired")
     }
 }
